a1a84257b39d4f08b0a852386bf403b66c952161
Mac <mac@MacBook-Pro.local>
2019-04-28 19:25:37 +0200

    Sample app using MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Layouts/Header';
+import Footer from './components/Layouts/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <h1>Start editing to see some magic happen {'\u2728'}</h1>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
